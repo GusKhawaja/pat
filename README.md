8aaf3027a5c852f3193c5ed0385766e8aa4e6816
GusKhawaja <gus.khawaja@guskhawaja.me>
2016-04-07 13:23:29 -0400

    Version 0.3

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# pat v0.2
+# pat v0.3
 This application will be the next generation for automating penetration testing tasks.
 The work is in progress.
--- pat.py
+++ pat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# This is version 0.2
+# This is version 0.3
 import webbrowser
 import time
 import os
@@ -7,44 +7,25 @@ import subprocess
 import argparse
 
 class Utilities:
+	# Common seperator line for the application
 	seperator_line = '------------------------------------------------------------'
 	
 	def __init__(self):
 		self.name = 'static class'
-
-	def usage(self):
-		print 'Pentester Automation Tool'
-		print Utilities.seperator_line
-		print 'Arguments:'
-		print '-c\t --company\t Your Client Company Domain Name'
-		print 
-		print 
-		print "Example:"
-		print "pat.py --company yourclientdomain.com"
-		print
-		#exit the application
-		exit(0)
 
 class Core:
 	def __init__(self,company_domain_name,utilities):
 		self.company_domain_name = company_domain_name
 		self.utilities = utilities
 
-	def _open_terminal(self,app_name,cmd):
-		output = '[+] Executing ' + str(app_name) + '...\r\n'
-		try:
-			output += subprocess.check_output(cmd,shell=True,stderr=subprocess.STDOUT)
-			output += '\r\n'
-		except Exception,e:
-			output += str(e)
-		output +=  self.utilities.seperator_line + '\r\n'
-		return output
-
+	# Description: Save the results to a file
+	# Return: (void)
 	def _save_results(self,results,folder_name,file_name):
 		try:
 			# Create a directory for the category e.g reconnissance or external scanning ...
 			if not os.path.isdir(folder_name):
 				os.mkdir(folder_name)
+				
 			# Save the results to a file
 			file_name = folder_name + '/'+ file_name
 			file_to_save = open(file_name,'w')
@@ -53,81 +34,147 @@ class Core:
 		except Exception,e:
 			print '[!] Error: Cannot save the results to a file!'
 
+	# Description: Open and execute Linux Terminal command
+	# Return: (string) return the results output after executing the command		
+	def _get_terminal_output(self,app_name,cmd):
+		output = '[+] Executing ' + str(app_name) + '...\r\n'
+		try:
+			output += subprocess.check_output(cmd,shell=True,stderr=subprocess.STDOUT)
+			output += '\r\n'
+		except Exception,e:
+			output += str(e)
+		output +=  self.utilities.seperator_line + '\r\n'
+		return output
+	
+	# Description: Iterate each command then open and execute Linux Terminal command
+	# Return: (string) return the results output after executing all the commands
 	def _execute_commands(self,commands):
 		#Execute commands in terminal
 		results = ''
 		for key,val in commands.items():
-			output = self._open_terminal(key,val)
+			output = self._get_terminal_output(key,val)
 			results += output
 		return results
 	
+	# Description: Iterate each website then open the web browser
+	# Return: (void) 
 	def _open_websites(self,websites):
 		# Open websites in browser
 		for website in websites:
 			webbrowser.open_new_tab(website)
+			# It's better to have a delay of 2 seconds between each new tab execution
 			time.sleep(2)
-
-	def _execute(self,commands,websites,folder_name,file_name):
+	
+	# Description: Start PenTest
+	# Return: (void)	
+	def _start(self,commands,websites,folder_name,file_name):
+		# If both lists are null then we stop here
 		if commands == None and websites == None:
-			print 'NO commands available'
+			print 'No commands available'
 			return
-
+		
+		# Execute and save terminal commands
 		if commands != None:
 			results = self._execute_commands(commands)
 			print results
 			self._save_results(results,folder_name,file_name)
 		
+		# Open websites
 		if websites != None:
 			self._open_websites(websites)
-			
-	def start(self):
-		# Print Banner
-		print 'Pentesting Client Domain Name: ' + self.company_domain_name
-		print self.utilities.seperator_line
-		print
 	
-		# Create a directory by Client Domain Name
+	# Description: Create a directory by Client Domain Name
+	# Return: (void)			
+	def _create_client_domain_folder(self):
 		if not os.path.isdir(self.company_domain_name):
 			os.mkdir(self.company_domain_name)
-
-	def check(self,action_name,folder_name):
-		# Get/Load the terminal commands from file if needed
+	
+	# Description: Get/Load the terminal commands from file if needed
+	# Return: (dictionary list) return the list of commands			
+	def _get_commands_from_file(self,action_name):
 		commands = {}
 		# e.g commands_file_path = 'dns_commands.txt'
-		commands_file_path = action_name+'_commands.txt'
-		
+		commands_file_path = action_name + '_commands.txt'
+	
 		if os.path.isfile(commands_file_path):
 			commands_file = open(commands_file_path,'r')
-
+	
 			for command_line in commands_file.readlines():
 				command_line_splitted = command_line.split(':')
 				commands[command_line_splitted[0]] = command_line_splitted[1]
-		
-		# Get/Load the websites from file if needed
+				
+		return commands
+	
+	# Description: Get/Load the websites from file if needed
+	# Return: (array) return the list of websites				
+	def _get_websites_from_file(self,action_name):
 		websites = []
 		# e.g websites_file_path= 'dns_websites.txt'
-		websites_file_path = action_name+'_websites.txt'
-		
+		websites_file_path = action_name + '_websites.txt'
+	
 		if os.path.isfile(websites_file_path):
 			websites_file = open(websites_file_path,'r')
 			for website_line in websites_file.readlines():
 				websites.append(website_line.strip('\r').strip('\n'))
 				
+		return websites
+	
+	# Description: Start penetration testing			
+	def pen_test(self,action_name,category_name):
+		# Create the Client Root directory
+		self._create_client_domain_folder()
+		
+		# get commands from file
+		commands = self._get_commands_from_file(action_name)
+		
+		#get websites from file
+		websites = self._get_websites_from_file(action_name)
+				
 		#e.g folder_name = 'test.com/reconnaissance'
-		folder_name = self.company_domain_name + '/' + folder_name 
-		#e.g file_name = 'dns.txt'
-		file_name = action_name + '.txt'
+		folder_name = self.company_domain_name + '/' + category_name
+		#e.g file_name = 'dns_report.txt'
+		file_name = action_name + '_report.txt'
 
-		self._execute(commands,websites,folder_name,file_name)
+		self._start(commands,websites,folder_name,file_name)
 
 class Main:
-	def _process_arguments(self,args,core,utilities):
+	def __init__(self,utilities):
+		self.utilities = utilities
+	
+	# Description: Print a help banner to show how to use the application and exit the application
+	# Return: (void)		
+	def _usage(self):
+		print 'Pentester Automation Tool'
+		print self.utilities.seperator_line
+		print 'Arguments:'
+		print '-c\t --company\t Your Client Company Domain Name'
+		print 
+		print 
+		print "Example:"
+		print "pat.py --company yourclientdomain.com"
+		print
+		#exit the application
+		exit(0)	
+		
+	# Description: Print a welcome banner
+	# Return: (void)		
+	def _print_banner(self,company_domain_name):
+		# Print Banner
+		print 'Pentesting Client Domain Name: ' + company_domain_name
+		print self.utilities.seperator_line
+		print		
+	
+	# Description: Process the terminal arguments
+	# Return: (void)	
+	def _process_arguments(self,args,core):
 		reconnaissance_category = 'Reconnaissance'
 		
 		if args.dns_test:
-			core.check('dns',reconnaissance_category)
-		
-	def start(self):
+			core.pen_test('dns',reconnaissance_category)
+	
+	# Description: Initialize the terminal arguments
+	# Return: (void)	
+	def _initialize_arguments(self):
 		# Arguments
 		parser = argparse.ArgumentParser('Pentester Automation Tool')
 		parser.add_argument("-c","--company",type=str,help="Your Client Company Domain Name")
@@ -136,19 +183,32 @@ class Main:
 		# Parse the arguments
 		args= parser.parse_args()
 	
-		# initialize the Utilities class
-		utilities = Utilities()
-		
+		# Do not proceed further if the company domain name is null
 		if args.company == None:
-			utilities.usage()
-	
-		core = Core(args.company,utilities)
-		core.start()
+			self._usage()		
 		
-		self._process_arguments(args,core,utilities)
+		return args
+	
+	# Description: Start the application
+	# Return: (void)	
+	def start(self):
+		# Terminal arguments
+		args = self._initialize_arguments()
+		
+		# Initialize the Core class	
+		core = Core(args.company,self.utilities)
+		
+		# Print application banner
+		self._print_banner(args.company)
+		
+		# Process the initilaized arguments
+		self._process_arguments(args,core)
 
 if __name__ == '__main__':
-	main = Main()
+	# Initialize the Utilities class
+	utilities = Utilities()	
+	
+	main = Main(utilities)
 	main.start()
 		
 
